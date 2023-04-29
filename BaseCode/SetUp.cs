ef7f985618e645fba27682ead891e7efa201cb50
vafaabadi <vafaabadi12@gmail.com>
2023-04-29 21:01:13 +0100

    Extent Reports populated now. Also, the test cases grouped to BeforeMidNight and NextDay.

---
+++ BaseCode/SetUp.cs
@@ -0,0 +1,50 @@
+﻿using AventStack.ExtentReports.Reporter;
+using AventStack.ExtentReports;
+using NUnit.Framework;
+using System;
+
+namespace WebUITestAutomation
+{
+
+
+
+    [SetUpFixture]
+    public class SetUp
+    {
+
+        string BaseDirectory_Path = AppDomain.CurrentDomain.BaseDirectory;
+
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+
+
+            String reportPath = BaseDirectory_Path; 
+            var htmlReporter = new ExtentHtmlReporter(reportPath);
+            Base.extent = new ExtentReports();
+            Base.extent.AttachReporter(htmlReporter);
+            Base.extent.AddSystemInfo("Host name", "Local host");
+            Base.extent.AddSystemInfo("Environment", "QA");
+            Base.extent.AddSystemInfo("User name", "Vafa Abadi");
+
+
+
+        }
+
+
+        [OneTimeTearDown]
+        public void AfterAllTests()
+        {
+            Base.extent.Flush();
+
+        }
+
+
+
+
+
+
+
+    }
+}
