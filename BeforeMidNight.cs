825ba2d41a171dca23b88aabb318c709a0c8d884
vafaabadi <vafaabadi12@gmail.com>
2023-05-23 23:46:22 +0100

    .

--- BeforeMidNight.cs
+++ BeforeMidNight.cs
@@ -67,8 +67,9 @@ namespace WebUITestAutomation
             driver.FindElementByXPath("//*[@id=\"omnibox-directions\"]/div/div[3]/div[2]/button/div").Click();
             Thread.Sleep(3000);
             //type in destination
-            driver.FindElementByXPath("//*[@id=\"sb_ifc52\"]/input").SendKeys("31 Springhead, Tunbridge Wells TN2 3NY");
+            driver.FindElementByCssSelector("#sb_ifc52 > input").SendKeys("31 Springhead, Tunbridge Wells TN2 3NY");
             Thread.Sleep(3000);
+
             InputSimulator sim2 = new InputSimulator();
             sim2.Keyboard.KeyPress(VirtualKeyCode.RETURN);
             Thread.Sleep(5000);
