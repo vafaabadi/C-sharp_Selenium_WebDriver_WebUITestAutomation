1d47b93aa1515e0f951f74f26ced6fdeb3b8c1f8
vafaabadi <vafaabadi12@gmail.com>
2023-06-02 21:48:10 +0100

    added example test case with POM

---
+++ LogIn_Pages/LogInPage.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium;
+
+namespace UnitTestProject1.LogIn_Pages
+{
+    public class LogInPage
+    {
+        public IWebDriver driver;
+
+        public LogInPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+
+        public IWebElement Username => driver.FindElement(By.XPath("//input[@name=\"username\"]"));
+
+        public IWebElement Password => driver.FindElement(By.XPath("//input[@name=\"password\"]"));
+
+        public IWebElement Submit => driver.FindElement(By.XPath("//input[@type='submit']"));
+
+    }
+}
---
+++ LogIn_Pages/SubmitPage.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+
+namespace UnitTestProject1.LogIn_Pages
+{
+    public class SubmitPage
+    {
+        public IWebDriver driver;
+
+        public SubmitPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+
+        public IWebElement ValueUsername => driver.FindElement(By.XPath("//*[@id=\"_valueusername\"]"));
+
+        public IWebElement ValuePassword => driver.FindElement(By.XPath("//*[@id=\"_valuepassword\"]"));
+
+
+    }
+}
